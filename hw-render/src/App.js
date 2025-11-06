b00fbe90bc88392dad628e99cd0c6f12e399bf0e
rs1291 <rayan.frostbyte@gmail.com>
2025-11-06 09:10:39 +0300

    inatial commit

---
+++ hw-render/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+const mycode = (
+  <table>
+    <tr>
+      <th>Customer Name</th>
+      <th>Customer City</th>
+      <th>Amount of Purchase</th>
+    </tr>
+    <tr>
+      <td>Praveen</td>
+      <td>Madurai</td>
+      <td>12000</td>
+    </tr>
+    <tr>
+      <td>Kumar</td>
+      <td>Chennai</td>
+      <td>10000</td>
+    </tr>
+  </table>
+);
+ReactDOM.render(mycode, document.getElementById('root'));
+export default App;
